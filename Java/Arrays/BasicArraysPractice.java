81dd42ff3ee12569f58dbab007ca4eb39004eb96
Code's Garden <72145188+Galaxy-Ranger@users.noreply.github.com>
2025-01-04 06:04:57 +0000

    till linear search.

---
+++ Java/Arrays/BasicArraysPractice.java
@@ -0,0 +1,33 @@
+import java.util.*;
+//* Arrays - List of elements of the "same type" placed in a "contiguous" memory location.
+//! Key Notes: Arrays pass in a function as "Call by Reference," so the changes made in an array are also reflected in the original array.
+
+//? Creation of an Array
+/*
+public class BasicArraysPractice {
+    public static void main(String[] args) {
+        
+        int arr1[] = new int[10];    //* Syntax - datatype varName[] = new Array[size];
+        System.out.println(arr[5]);
+
+        String name[] = {"Surya", "Rohit", "Robin"};   //* Syntax - datatype varName[] = {element, ...};
+        System.out.println(name[0]);
+    }
+}
+*/
+//? Input/Output in an Array
+/*
+public class BasicArraysPractice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int marks[] = new int[5];
+        for(int i=0; i<marks.length; i++) {
+            marks[i] = sc.nextInt();  //* input in an array.
+        }
+        for(int i=0; i<marks.length; i++) {
+            System.out.print(marks[i] + " ");  //* output from an array.
+        }
+    }
+}
+*/
