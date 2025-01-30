b303ab85009d0c8480084b08f01aa6268f5accf0
Code's Garden <72145188+Galaxy-Ranger@users.noreply.github.com>
2025-01-30 10:29:10 +0530

    pattern1

---
+++ Take You Forward(DSA Sheet)/Lec-2_Build-up_Logical_Thinking/Patterns.py
@@ -0,0 +1,14 @@
+# Pattern - 1
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+
+n = int(input("Enter Number for Pattern: "))
+
+def pattern1(n):
+  for i in range(n):
+    print("* * * * *")
+
+pattern1()
