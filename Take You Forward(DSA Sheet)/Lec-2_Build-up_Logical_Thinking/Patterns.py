70e2b0f9919241c9111b5eba4bdfb9931e4399f6
Code's Garden <72145188+Galaxy-Ranger@users.noreply.github.com>
2025-01-30 14:53:05 +0530

    half of the pattern questions finished.

--- Take You Forward(DSA Sheet)/Lec-2_Build-up_Logical_Thinking/Patterns.py
+++ Take You Forward(DSA Sheet)/Lec-2_Build-up_Logical_Thinking/Patterns.py
@@ -107,3 +107,86 @@ def pattern7(n):
       print("*", end=" ")
     print()
 pattern7(n)
+print()
+
+# Pattern - 8
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+
+def pattern8(n):
+  for i in range(n, 0 , -1):
+    # spaces
+    for space in range(n - i):
+      print(end="  ")
+    # stars
+    for star in range(1, 2 * i):
+      print("*", end=" ")
+    print()
+pattern8(n)
+print()
+
+# Pattern - 9
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+
+def pattern9(n):
+  pattern7(n)
+  for i in range(n-1, 0, -1):
+    # spaces
+    for space in range(n - i):
+      print(end="  ")
+    # stars
+    for star in range(1, 2 * i):
+      print("*", end=" ")
+    print()
+pattern9(n)
+print()
+
+# Pattern - 10
+# *
+# **
+# ***
+# **
+# *
+
+def pattern10(n):
+  for i in range(n):
+    for j in range(i + 1):
+      print("*", end=" ")
+    print()
+  for i in range(n-1, 0, -1):
+    for j in range(i):
+      print("*", end=" ")
+    print()
+pattern10(n)
+print()
+
+# Pattern - 11
+#* 1
+#* 0 1
+#* 1 0 1
+#* 0 1 0 1
+#* 1 0 1 0 1
+
+def pattern11(n):
+  for i in range(1, n + 1):
+    for j in range(1, i + 1):
+      if((i + j) % 2 == 0):
+        print(1, end=" ")
+      else:
+        print(0, end=" ")
+    print()
+pattern11(n)
+print()
+
+# Pattern - 12
