f0903232177894b034e3ca01f90dfe5229b5ec70
Galaxy-Ranger <surya.p.s780@gmail.com>
2025-02-04 11:11:11 +0530

    just few comments added

--- Take You Forward(DSA Sheet)/Step-2 Learn Important Sorting Techniques/Lec-1_Sorting-1/03_inserting_sort.py
+++ Take You Forward(DSA Sheet)/Step-2 Learn Important Sorting Techniques/Lec-1_Sorting-1/03_inserting_sort.py
@@ -2,6 +2,12 @@ L = [74, 32, 89, 55, 21, 64]
 
 print("Before Insertion Sort: ", L)
 
+#? Approach:
+
+#* Select an element in each iteration from the unsorted array(using a loop).
+#* Place it in its corresponding position in the sorted part and shift the remaining elements accordingly (using an inner loop and swapping).
+#* The “inner while loop” basically shifts the elements using swapping.
+
 for i in range(1, len(L)):
     j = i
     while j > 0 and L[j] < L[j - 1]:
@@ -9,3 +15,8 @@ for i in range(1, len(L)):
         j -= 1
 
 print("After Insertion Sort: ", L)
+
+#* Time Complexity
+#? Worst case ---> O(n^2)
+#? Best case ---> O(n)
+#? Average case ---> O(n^2)
